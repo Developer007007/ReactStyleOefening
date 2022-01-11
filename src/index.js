889a8e8cf95fcfd08b9aba3749f9368d0a45d83d
Developer007007 <dvlprconnect@gmail.com>
2022-01-12 00:10:40 +0100

    React-Style-oefenong

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+
+const img = "https://picsum.photos/200";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <div>
+    {/* <h1 className="heading" contentEditable="false">My Favourite Foods</h1> */}
+    <h1 className="heading" contentEditable="true" spellcheck="false">
+      My Favourite Foods
+    </h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <div>
+      
+      <img className="food-img" src="https://mobkitchen-objects.imgix.net/recipes/2021/849A3323.jpg?auto=format&crop=focalpoint&domain=mobkitchen-objects.imgix.net&fit=crop&fp-x=0.5&fp-y=0.5&h=1300&ixlib=php-3.3.1&q=82&w=1300&s=ad18ec26d2bedbe0019b3d87fecfeffb"></img>
+      <img className="food-img" src="https://vvvv.org/sites/default/files/Fresh_ramen_noodle_001.jpg"></img>
+      <img className="food-img" src="https://purewows3.imgix.net/images/articles/2022_01/easy-asian-noodle-recipes_Dan-Dan-Noodles_4-2.jpeg?auto=format,compress&cs=strip"></img>
+
+
+      {/* https://picsum.photos/ */}
+      <img  src={img}></img>
+      <img  src={img + "?grayscale"}></img>
+
+    </div>
+  </div>,
+  document.getElementById("root")
+);
